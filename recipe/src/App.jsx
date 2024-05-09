93d214c633ec5de84861e16053fabe5b74f96146
John Washington <johnwashinton88@gmail.com>
2024-05-09 16:31:05 +0100

    initializing

---
+++ recipe/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import RecipeList from './components/RecipeList';
+// import MealDailog from './components/MealDailog';
+import './App.css'
+
+const App = () => {
+  return (
+    
+      <>
+      <h1 className='h1'> Recipe App</h1>
+      <RecipeList />
+      
+      </>
+  );
+};
+
+export default App;
+
+
+
+
+
